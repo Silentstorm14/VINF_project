5119669ffd0fcb3029636421df74cb28d257e3cd
Erik Podola <erik.earl14@gmail.com>
2021-10-19 07:10:44 +0200

    konzultacia 1

---
+++ Review.py
@@ -0,0 +1,10 @@
+class Review:
+    def __init__(self,id, product, reviewer, helpful, overall, reviewText, reviewTime,summary):
+        self.id = id
+        self.product = product
+        self.reviewer = reviewer
+        self.helpful = helpful
+        self.overall = overall
+        self.reviewText = reviewText
+        self.reviewTime = reviewTime
+        self.sumary = sumary
